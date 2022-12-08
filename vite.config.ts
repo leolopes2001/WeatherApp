cb9767828cc71f5d12b042a45cd0d40535c204fa
leolopes2001 <leomartins2001@gmail.com>
2022-12-08 16:53:10 -0300

    Vercel bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+
+
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    plugins: [reactRefresh(), eslintPlugin({ cache: false })],
+    root: 'src',
+    build: {
+        outDir: '../dist'
+    }
 })
