ddb8cb2db04dbcf83e80e46a77d89fb4656262d5
leolopes2001 <leomartins2001@gmail.com>
2022-12-26 11:21:35 -0300

    Fix api bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,6 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
-
-
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import eslintPlugin from 'vite-plugin-eslint'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [reactRefresh(), eslintPlugin({ cache: false })],
-    root: 'src',
-    build: {
-        outDir: '../dist'
-    }
+  plugins: [react()]
 })
